5dd88acc2102ba52f4c66d0399e4908d16d03a91
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-18 15:49:16 -1000

    get/post functions ready for users/tasks, but postman tests are failing

--- server.js
+++ server.js
@@ -1,22 +1,51 @@
 var express = require('express');
-var app = express();
 // body-parser package is required to convert HTML reqs into objects that JS can work with.
 var bodyParser = require('body-parser');
 
 // Access the User model from the db object.
 var db = require('./models');
 var User = db.User;
+var Task = db.Task;
+
+var app = express();
 
 // Handle post data.
 // 1. Create a user.
-app.post('/user', function (req, res) {
-  db.User.create({username: req.body.username})
-    .then(function (user) {
-      res.json(user);
+app.post('/users', function (req, res) {
+  db.User.create({
+    username: req.body.username
+  }).then(function (user) {
+    res.json(user);
+  });
+});
+
+// 2. Create a task.
+app.post('/tasks', function (req, res) {
+  db.Task.create({
+    title: req.body.title
+  }).then(function (task) {
+    res.json(task);
+  });
+});
+
+// Handle get data
+// 1. Return all users
+app.get('/users', function (req, res) {
+  User.findAll()
+    .then(function (users) {
+      res.json(users);
+    });
+});
+
+// 2. Return all tasks
+app.get('/tasks', function (req, res) {
+  Task.findAll()
+    .then(function (tasks) {
+      res.json(tasks);
     });
 });
 
 app.listen(3000, function () {
   db.sequelize.sync();
-  console.log('Server listening on 3000. Connected to database sequelize_express');
+  console.log('Listening on 3000. Connected to database sequelize_express.');
 });
